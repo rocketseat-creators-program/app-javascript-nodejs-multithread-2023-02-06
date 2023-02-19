6dc848b8f14ae4016bcd551c63a166aca0ae0ffa
Sergio Cabral <sergio@gohorse.dev>
2023-02-19 00:03:58 -0300

    feat: separado código em main e sub-thread

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 /**
  * Sintaxe:
- *   node index.js 000 rocketseat
+ *   node index.js 3 000 rocketseat
  */
 
-const prefix = process.argv[2];
-const input = process.argv.slice(3).join(" ");
+const { isMainThread } = require('worker_threads');
 
-const { createHash } = require("crypto");
-const { freemem, totalmem } = require("os");
-let nonce = 0;
-let hash;
-do {
-    nonce++;
-    hash = createHash('sha256').update(`${input}${nonce}`).digest('hex');
-    memory = ((1 - freemem() / totalmem()) * 100).toFixed(2) + '%';
-    console.log(`Input: ${input} / Nonce: ${nonce} / Hash: ${hash} / Memory: ${memory}`);
-} while (!hash.startsWith(prefix));
+
+if (isMainThread) {
+    const threads = process.argv[2];
+    const prefix = process.argv[3];
+    const input = process.argv.slice(4).join(" ");
+
+    console.log(`threads: ${threads} / prefix: ${prefix} / input: ${input}`);
+} else {
+    const prefix = "??";
+    const input = "??";
+
+    const { createHash } = require("crypto");
+    const { freemem, totalmem } = require("os");
+    let nonce = 0;
+    let hash;
+    do {
+        nonce++;
+        hash = createHash('sha256').update(`${input}${nonce}`).digest('hex');
+        memory = ((1 - freemem() / totalmem()) * 100).toFixed(2) + '%';
+        console.log(`Input: ${input} / Nonce: ${nonce} / Hash: ${hash} / Memory: ${memory}`);
+    } while (!hash.startsWith(prefix));
+}
