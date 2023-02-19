09bcbbc5fdb0558e27fbe5935a5b7f53c9956352
Sergio Cabral <sergio@gohorse.dev>
2023-02-19 00:28:13 -0300

    feat: finalizando outras threads

--- index.js
+++ index.js
@@ -3,16 +3,24 @@
  *   node index.js 3 000 rocketseat
  */
 
-const { isMainThread, Worker, workerData } = require('worker_threads');
-
+const { isMainThread, Worker, workerData, parentPort } = require('worker_threads');
 
 if (isMainThread) {
     const threads = process.argv[2];
     const prefix = process.argv[3];
     const input = process.argv.slice(4).join(" ");
 
+    const workers = [];
     for (let i = 0; i < threads; i++) {
-        new Worker(__filename, { workerData: { prefix, input } });
+        const worker = new Worker(__filename, { workerData: { prefix, input } });
+        workers.push(worker);
+        worker.on('message', message => {
+            if (message === "END") {
+                workers
+                    .filter(otherWorker => otherWorker !== worker)
+                    .forEach(otherWorker => otherWorker.terminate());
+            }
+        });
     }
 } else {
     const prefix = workerData.prefix;
@@ -28,4 +36,5 @@ if (isMainThread) {
         memory = ((1 - freemem() / totalmem()) * 100).toFixed(2) + '%';
         console.log(`Input: ${input} / Nonce: ${nonce} / Hash: ${hash} / Memory: ${memory}`);
     } while (!hash.startsWith(prefix));
+    parentPort.postMessage('END');
 }
