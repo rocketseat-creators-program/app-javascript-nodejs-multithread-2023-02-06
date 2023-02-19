6d7e472164fffab083d75745bd02304d0c6c520e
Sergio Cabral <sergio@gohorse.dev>
2023-02-18 23:51:01 -0300

    feat: consumo de memória

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const prefix = process.argv[2];
 const input = process.argv.slice(3).join(" ");
 
 const { createHash } = require("crypto");
+const { freemem, totalmem } = require("os");
 let nonce = 0;
 let hash;
 do {
     nonce++;
     hash = createHash('sha256').update(`${input}${nonce}`).digest('hex');
-    console.log(`Input: ${input} / Nonce: ${nonce} / Hash: ${hash}`);
+    memory = ((1 - freemem() / totalmem()) * 100).toFixed(2) + '%';
+    console.log(`Input: ${input} / Nonce: ${nonce} / Hash: ${hash} / Memory: ${memory}`);
 } while (!hash.startsWith(prefix));
